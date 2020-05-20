b227a816f07bd3fead630a2ae87c4272c78a10b5
Administrator <Administrator@PC-20171209OOKW>
2020-05-20 11:24:02 +0800

    重构了服务端

--- AutoTradingRobotDeamon/TcpServer.cpp
+++ AutoTradingRobotDeamon/TcpServer.cpp
@@ -1,25 +1,59 @@
 #include "TcpServer.h"
-#include <QDebug>
+#include "tcp_server_private.h"
+//构造函数
+TcpServer::TcpServer(QObject *parent)
+	: QTcpServer(parent),
+	private_(new tcp_server_private::TcpServerPrivate) {
+}
 
-TcpServer::TcpServer()
-{
-	tcpServer = new QTcpServer(this);
-	if (!tcpServer->listen(QHostAddress::LocalHost, 13888))
-	{   //监听本地主机的13888端口，如果出错就输出错误信息，并关闭
-		qDebug() << tcpServer->errorString();
+//析构函数
+TcpServer::~TcpServer() {
+	for (tcp_server_private::TcpSocket *client : private_->clients.values()) {
+		client->disconnectFromHost();
+		auto handle = client->socketDescriptor();
+		client->deleteLater();
+		//告知其他调用者 当前socket断开，避免有需要在socket后执行的方法
+		emit ClientDisconnected(handle);
 	}
-	//连接信号和相应槽函数
-	connect(tcpServer, SIGNAL(newConnection()), this, SLOT(newConnection()));
-	qDebug() << "------------------------------------------------------";
-	qDebug() << "--> AutoTradingRobotDeamon v1.0 started.";
+	if (private_)
+		delete private_;
+	this->close();
 }
 
+//重写-有连接到来
+void TcpServer::incomingConnection(qintptr handle) {
+	//超出最大练级数量关闭连接
+	if (private_->clients.size() > maxPendingConnections()) {
+		QTcpSocket tcp;
+		tcp.setSocketDescriptor(handle);
+		tcp.disconnectFromHost();
+		return;
+	}
+	auto client_socket = new tcp_server_private::TcpSocket(handle);
+	Q_ASSERT(client_socket->socketDescriptor() == handle);
 
-TcpServer::~TcpServer()
-{
+	//socket断开连接的信号与server槽连接
+	connect(client_socket,
+		&tcp_server_private::TcpSocket::ClientDisconnected,
+		this,
+		&TcpServer::ClientDisconnectedSlot);
+
+	//主动断开连接的操作
+	connect(this,
+		&TcpServer::InitiativeDisConnectClient,
+		client_socket,
+		&tcp_server_private::TcpSocket::DisconnectSocket);
+	//map记录
+	private_->clients.insert(handle, client_socket);
+	qDebug() << handle << "connected";
+	emit ClientConnected(handle, client_socket);
 }
 
-void TcpServer::newConnection()
-{
-	qDebug() << "new connection.";
+//客户端已断开槽
+void TcpServer::ClientDisconnectedSlot(qintptr handle) {
+	//map中移除
+	private_->clients.remove(handle);
+	qDebug() << handle << "disconnected";
+	//发出信号
+	emit ClientDisconnected(handle);
 }
--- AutoTradingRobotDeamon/main.cpp
+++ AutoTradingRobotDeamon/main.cpp
@@ -6,6 +6,14 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 	TcpServer server;
-	// 
+
+	if (!server.listen(QHostAddress::Any, 13888))
+	{   //监听本地主机的13888端口，如果出错就输出错误信息，并关闭
+		qDebug() << server.errorString();
+	}
+	
+	qDebug() << "-----------------------------------------------------";
+	qDebug() << "-->Server has launched.";
+	qDebug() << "-->Server port:13888";
 	return a.exec();
 }
